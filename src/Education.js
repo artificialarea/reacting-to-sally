3f09e642907b5d44c4388c4b316f635cfdb7659c
Sacha Sedriks <sachased@gmail.com>
2020-05-05 21:10:56 -0700

    adding education.js

---
+++ src/Education.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Education() {
+  return (
+    <section id="education">
+      <h2>Education</h2>
+      <h3>San Francisco Art Institute</h3>
+      <p>2016</p>
+      <p>Post-Baccalaureate, New Genres</p>
+      <h3>Middlebury College</h3>
+      <p>2015</p>
+      <p>Bachelors, Humanities & Liberal Arts</p>
+    </section>
+  );
+}
+
+export default Education;
