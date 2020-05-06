d1da5cd0c0aeccaa49dd395808c5d35a3f8757e4
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-05-05 20:57:18 -0700

    add experience component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Experience from "./Experience";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg"
+          alt="Photo of Sally Student"
+          width="134"
+        />
+        <h1>Sally Student</h1>
       </header>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <Experience />
+
+        <section id="education">
+          <h2>Education</h2>
+          <h3>San Francisco Art Institute</h3>
+          <p>2016</p>
+          <p>Post-Baccalaureate, New Genres</p>
+          <h3>Middlebury College</h3>
+          <p>2015</p>
+          <p>Bachelors, Humanities & Liberal Arts</p>
+        </section>
+
+        <h2>Contact</h2>
+        <address id="contact">
+          <ul>
+            <li>
+              <b>Email address:</b>{" "}
+              <a
+                href="mailto:sallystudent@gmail.com?subject=Hi%20Sally!"
+                target="_blank"
+              >
+                sallystudent@gmail.com
+              </a>
+            </li>
+            <li>
+              <b>Phone Number:</b> (650) 555-3452
+            </li>
+          </ul>
+        </address>
+      </main>
     </div>
   );
 }
---
+++ src/Experience.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Experience() {
+  return (
+    <section id="experience">
+      <h2>Experience</h2>
+      <h3>Interaction Designer</h3>
+      <h4>AKQA</h4>
+      <p>2018-Present</p>
+      <p>
+        Duties included: Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <h3>Junior Interaction Designer</h3>
+      <h4>R/GA</h4>
+      <p>2017-2018</p>
+      <p>
+        Duties included: Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </section>
+  );
+}
+
+export default Experience;
