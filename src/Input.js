e3b93d82a867c70702a0a95d02444ffeafbcc372
Sacha Sedriks <sachased@gmail.com>
2020-05-08 13:11:55 -0700

    fixed the typeerror of 'submit' undefined. It was due to a spelling error (naturally).

--- src/Input.js
+++ src/Input.js
@@ -10,32 +10,34 @@ class InputForm extends React.Component {
     // In JavaScript, 'super' refers to the parent class constructor. (In our example, it points to the React.Component implementation.)
     // You can’t use 'this' in a constructor until after you’ve called the parent constructor.
     // For details: https://overreacted.io/why-do-we-write-super-props/
-    
+
+    // Don't call this.setState() here within the constructor()!
+    // Instead, assign the initial state like so...
     this.state = {value: ''};
 
+    console.log(this.state);
     this.handleChange = this.handleChange.bind(this);
-    this.handleSumbit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
+    console.log(event.target.value);
     this.setState({value: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    // console.log('Something was submitted: ' + this.state.value);
-    alert('Something was submitted: ' + this.state.value);
-    // alert('Something was submitted');
+    console.log('Something was submitted: ' + this.state.value);
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-        Whatever:
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <input type="submit" value="submit" />
+        <input type="submit" value="Submit" />
       </form>
     );
   } 
